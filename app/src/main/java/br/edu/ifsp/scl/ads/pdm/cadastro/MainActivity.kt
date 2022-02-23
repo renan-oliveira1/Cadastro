52a47f8ad37ec5af75d3d92d1af8f7bd61a853c2
renan.oliveira <renan.oliveira.rao@gmail.com>
2022-02-23 10:46:20 -0300

    implemented clear fields

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/cadastro/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/cadastro/MainActivity.kt
@@ -2,9 +2,10 @@ package br.edu.ifsp.scl.ads.pdm.cadastro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.edu.ifsp.scl.ads.pdm.cadastro.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,5 +15,34 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setClickListeners()
+
+    }
+
+
+    private fun clearFields(){
+        binding.editName.text.clear()
+        binding.editPhone.text.clear()
+        binding.editEmail.text.clear()
+        binding.checkbox.isChecked = false
+        binding.radioGroupSex.clearCheck()
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        if(id == binding.buttonClear.id){
+            clearFields()
+        }else if(id == binding.buttonSave.id){
+
+        }
+    }
+
+    private fun setClickListeners(){
+
+        binding.buttonClear.setOnClickListener(this)
+
+        binding.buttonSave.setOnClickListener(this)
+
     }
 }
